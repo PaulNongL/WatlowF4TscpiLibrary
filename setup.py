70e9b19194fcb092bcb5d4bc29a6f2f59f6b3c28
Paul Nong-Laolam <38042735+PaulNongL@users.noreply.github.com>
2022-10-10 14:31:04 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+'''setup script for this module'''
+
+from setuptools import setup
+
+def readme():
+    '''pull iin the readme file for the long description'''
+    with open('README.md') as rfile:
+        return rfile.read()
+
+setup(
+    name='f4tscpi',
+    version='0.1.0',
+    description='A library for interfacing with Watlow F4T using SCPI method',
+    long_description=readme(),
+    url='https://github.com/PaulNongL/WatlowF4TscpiLibrary/tree/f4tscpi',
+    author='Espec North America',
+    author_email='pnong-laolam@espec.com',
+    license='MIT',
+    packages=['f4tscpi'],
+    zip_safe=False,
+    keywords='F4T',
+    include_package_data=True,
+    scripts=['bin/f4t_run.py'],
+
+    classicifiers=[
+        'Programming Language :: Python :: 3.6.8',
+        'Programming Language :: Python :: 3.7.3',
+        'Programming Language :: Python :: 3.9.3',
+    ]
+)
