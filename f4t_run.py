0f99d7f64c4c9e46a0a2d6d1273625c2c44de1ba
Paul Nong-Laolam <38042735+PaulNongL@users.noreply.github.com>
2022-10-10 14:28:36 -0400

    Update f4t_run.py

--- f4t_run.py
+++ f4t_run.py
@@ -1,6 +1,6 @@
-#!/bin/python3
+#!python
 '''
-:author: Paul Nong-Laolam <paul.nong-laolam@espec.com>
+:author: Paul Nong-Laolam <pnong-laolam@espec.com>
 :license: MIT, see LICENSE for more detail.
 :copyright: (c) 2022. ESPEC North America, INC.
 :file: f4t_run.py 
@@ -13,12 +13,12 @@ from its class and method definitions.
 TCP/IP protocol is applied. 
 '''
 import os, sys, re
-sys.path.insert(0,'./f4t')
+sys.path.insert(0,'../f4tscpi')
 import time
 import logging
 
-from f4t.f4t_class import Controller, TempUnits, RampScale
-from f4t.f4t_interface import F4T
+from f4tscpi.f4t_class import Controller, TempUnits, RampScale
+from f4tscpi.f4t_interface import F4T
 
 LOG = logging.getLogger(__name__)
 
@@ -28,6 +28,7 @@ def ip_addr():
     while True:
         try:
             ip_addr = input('Enter F4T IP address (e.g., 192.168.0.101): ')
+            #ip_addr = '10.30.100.50'
             chk_ip = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip_addr)
             if chk_ip:
                 print ('\n')
@@ -64,10 +65,9 @@ def listProg():
     print ('\nReading profiles from F4T. Profile gap in the list is ignored.')
     print ("Profile list may be inaccurate if list is acquired while a profile is being executed.")
     print ("Please wait...")
+    time.sleep(0.5)
     tst.get_profiles()
-    tst.get_units()
     print (f"\nF4T profiles found in sequence (slot number: 'name'): \n{tst.profiles}")
-    print (f"\nTemperature Units currently used: \n   [Units]: {tst.temp_units}")
     pass 
 
 def listTempPV(loop):
@@ -142,14 +142,75 @@ def setTS():
     except ValueError:
         print ('Invalid TS number.')
 
+def tsName():
+    '''get Time Signal name
+    '''
+    try:
+        ts_num = int(input('Enter TS number: '))
+        if isinstance(ts_num, int):
+            tst.get_tsName(ts_num)
+    except ValueError:
+        print ('Invalid TS number.')
+
 def instantChange(mode, loop):
     '''Start the instant temperature change to set point
     '''
     print ('Starting Instant Change on temperature...')
     time.sleep(0.5)
-    tst.ramp_mode('STARTUP',1)
-    time.sleep(1)
+    #tst.ramp_mode('STARTUP',1)
+    #time.sleep(0.5)
     tst.ramp_mode(mode,loop)
+
+def ramp2SP(mode,loop):
+    '''Start ramp to Temp to set point
+    '''
+    print ('Starting Ramping on temperature to SetPoint value...')
+    time.sleep(0.5)
+    tst.ramp_mode(mode,loop)
+
+def setScale(loop):
+    '''Set ramp scale in unit or hours or unit of minutes
+
+    Unit: HOURS; MINUTES
+    '''
+    try: 
+        scale = input('Enter ramp scale type in M or H: ')
+        if scale == 'H':
+            time.sleep(0.5) 
+            tst.set_rampScale('HOURS',loop)
+        elif scale == 'M':
+            time.sleep(0.5)
+            tst.set_rampScale('MINUTES',loop)
+        else:
+            print ('Invalid scale type. Expecting letter "M" or "H".')
+    except ValueError:
+        print ('Invalid input.') 
+
+def setRV(loop):
+    '''Set new value for ramp rate
+    '''
+    try:
+        value = float(input('Enter rate value: '))
+        if isinstance(value, (int, float)):
+            tst.set_ramp('rate',value, loop)
+        else:
+            print ('Value must be a decimal number.') 
+    except ValueError:
+        print ('Invalid operation. Option terminated.')  
+
+def setRT(loop):
+    '''Set new value for ramp time
+
+    Time: house or minutes 
+    '''
+    try:
+        value = int(input('Enter time value in [MINUTES] unit: '))
+        if isinstance(value, int):
+            tst.set_ramp('time',value, loop)
+        else:
+            print ('Value must be integer (whole) number.') 
+    except ValueError:
+        print ('Invalid operation. Option terminated.')   
 
 def thCtrl():
     '''
@@ -166,17 +227,49 @@ def thCtrl():
             setTemp('Temp',1)
         elif option == 'h':
             setTemp('Humi',2)
-        elif option == 's':
-            instantChange('OFF',1)
+        elif option == 'l':
+            listTempPV(1)
         elif option == 'z':
             print('Returning to Main Menu.')
             time.sleep(.5)
             os.system('clear||cls')
-            selection()
+            main_menu()
         else:
             print('Invalid option; expected a letter [a-z].')
 
-def selection(): 
+def unit():
+    '''read device unit
+    '''
+    while(True):
+        print_menu('6')
+        option = ''
+        try:
+            option = input('Select option (r, s, z): ')
+        except:
+            print('Invalid input; expected a letter [r,s,z].')
+        if option == 'r':
+            print ('Probing device for Temp unit...')
+            time.sleep(0.5)
+            print (f'Temperature unit: {tst.get_units()}')
+        elif option == 's':
+            time.sleep(0.5)
+            tst.set_units()
+        elif option == 'z':
+            print('Returning to Main Menu.')
+            time.sleep(.5)
+            os.system('clear||cls')
+            main_menu()
+        else:
+            print('Invalid option; expected a letter [r,s,z].')
+
+def deviceID():
+    '''read device id information
+    '''
+    print ('Probing target device...')
+    time.sleep(0.5)
+    print (f'Manufacturer, Part Number, S/N, Software Version:\n{tst.get_id()}')
+
+def main_menu(): 
     '''
        Set options for program control
     '''
@@ -184,15 +277,21 @@ def selection():
         print_menu('1')
         option = ''
         try:
-            option = input('Select option (a-z): ')
+            option = input('Select option (i, t, p, ...): ')
         except:
             print('Invalid input; expected a letter [a-z].')
-        if option == 't':
+        if option == 'i':
+            deviceID()
+        elif option == 't':
             thCtrl()
         elif option == 'p':
             progMenu()
         elif option == 'e':
             eventCtrl()
+        elif option == 'r':
+            rampMenu()
+        elif option == 'u':
+            unit() 
         elif option == 'z':
             print('Program terminated.')
             exit()
@@ -213,14 +312,96 @@ def eventCtrl():
             readTS()
         elif option == 's':
             setTS()
+        elif option == 'n':
+            tsName()
         elif option == 'z':
             print('Return to Main Menu...')
             time.sleep(0.5)
             os.system('clear||cls')
-            selection() 
+            main_menu() 
         else:
             print('Invalid option; expected a letter [a-z].')
 
+def rampMenu():
+    '''define ramp mode and control
+        'rr': 'Read ramp rate value 
+        'sr': 'Set ramp rate value
+        'rt': 'Read ramp time value 
+        'st': 'Set ramp time value  
+        'rs': 'Ramp to SetPoint     
+        'is': 'Instant change to SP
+        'sc': 'Set Ramp Scale in [HR or MIN]
+        'z' : 'Return to Main Menu 
+    '''
+    while(True):
+        print_menu('5')
+        option = ''
+        chk_range = range(1,4,1)
+        try:
+            option = input('Select option ("rr", "sr", ...): ')
+        except:
+            print('Invalid input.')
+        if option == 'rr':
+            try:
+                loop = int(input('Enter loop number (1=Temp, 2=Humi; loop_max=4): '))
+                if isinstance(loop, int):
+                    if loop in chk_range:
+                        tst.get_ramp('rate',loop)
+                    else:
+                        print ('Loop out of range.') 
+            except ValueError:
+                print ('Invalid loop number.')  
+        elif option == 'sr':
+            try:
+                loop = int(input('Enter loop number (1=Temp, 2=Humi; loop_max=4): '))
+                if isinstance(loop, int):
+                    if loop in chk_range:
+                        setRV(loop)
+                    else:
+                        print ('Loop out of range.') 
+            except ValueError:
+                print ('Invalid loop number.') 
+        elif option == 'rt':
+            try:
+                loop = int(input('Enter loop number (1=Temp, 2=Humi; loop_max=4): '))
+                if isinstance(loop, int):
+                    if loop in chk_range:
+                        tst.get_ramp('time',loop)
+                    else:
+                        print ('Loop out of range.')  
+            except ValueError:
+                print ('Invalid loop number.')
+        elif option == 'st':
+            try:
+                loop = int(input('Enter loop number (1=Temp, 2=Humi; loop_max=4): '))
+                if isinstance(loop, int):
+                    if loop in chk_range:
+                        setRT(loop)
+                    else:
+                        print ('Loop out of range.') 
+            except ValueError:
+                print ('Invalid loop number.')             
+        elif option == 'rs':
+            ramp2SP('SETPOINT',1)
+        elif option == 'ic':
+            instantChange('OFF',1)
+        elif option == 'sc':
+            try:
+                loop = int(input('Enter loop number (1=Temp, 2=Humi; loop_max=4): '))
+                if isinstance(loop, int):
+                    if loop in chk_range:
+                        setScale(loop)
+                    else:
+                        print ('Loop out of range.') 
+            except ValueError:
+                print ('Invalid loop number.')
+        elif option == 'z':
+            print('Return to Main Menu...')
+            time.sleep(0.5)
+            os.system('clear||cls')
+            main_menu() 
+        else:
+            print('Invalid option.')
 
 def progMenu():  # test 
     '''set up selection menu for operation
@@ -254,46 +435,76 @@ def progMenu():  # test
             print('Return to Main.')
             time.sleep(0.5)
             os.system('clear||cls')
-            selection()
+            main_menu()
         else:
             print('Invalid option; expected a letter [a-z].')
 
 def menu(choice):
-    '''menu
+    '''menu list
+
+    option: 
+       1: main menu
+       2: Temp/Humi menu
+       3: Program menu
+       4: Output (Time Signal) menu
+       5: Ramp menu
+       6: Unit menu
     '''
-    # option 1
+    # main menu 
     main_menu = {
-        't': 'Temp/Humi SP control       ',
-        'p': 'Program control            ',
-        'e': 'Event control              ',
-        'z': 'Exit                       '
+        'i': 'Device information            ',
+        't': 'Temp/Humi SP control          ',
+        'p': 'Program control               ',
+        'r': 'Ramp control                  ',
+        'e': 'Event control                 ',
+        'u': 'Temperature Unit              ',
+        'z': 'Exit                          '
     }
 
-    # option 2
+    # temp and humi ctrl menu
     th_menu = {
-        't': 'New Temperature Set Point  ',
-        'h': 'New Humidity Set Point     ',
-        's': 'Start instant change to SP ',
-        'z': 'Return to Main Menu        '
+        't': 'New Temperature Set Point     ',
+        'h': 'New Humidity Set Point        ',
+        'l': 'List current Temp PV/SP       ',
+        'z': 'Return to Main Menu           '
     }
 
-    # option 3
+    # program menu 
     prog_menu = {
-        'l': 'List program               ',
-        'e': 'Execute program            ',
-        'p': 'Pause program              ',
-        'r': 'Resume program             ',
-        's': 'Stop program               ',
-        'z': 'Return to Main Menu        '
+        'l': 'List program                  ',
+        'e': 'Execute program               ',
+        'p': 'Pause program                 ',
+        'r': 'Resume program                ',
+        's': 'Stop program                  ',
+        'z': 'Return to Main Menu           '
     }
 
-    # option 4
+    # event ctrl menu 
     ts_menu = {
-        'r': 'Read event (TS) output     ',
-        's': 'Set event (TS) output      ', 
-        'z': 'Return to Main Menu        '
+        'r': 'Read event (TS) output        ',
+        's': 'Set event (TS) output         ', 
+        'n': 'Read event (TS) name          ',
+        'z': 'Return to Main Menu           '
     }
 
+    # ramp ctrl menu 
+    ramp_menu = {
+        'rr': 'Read ramp rate value         ',
+        'sr': 'Set ramp rate value          ', 
+        'rt': 'Read ramp time value         ',
+        'st': 'Set ramp time value          ',
+        'rs': 'Ramp to SetPoint             ',
+        'ic': 'Instant change to SP         ',
+        'sc': 'Set Ramp Scale in H or M     ', 
+        'z ': 'Return to Main Menu          '
+    }
+
+    # unit menu 
+    unit_menu = {
+        'r': 'Read Temp unit                ',
+        's': 'Set new Temp unit             ', 
+        'z': 'Return to Main Menu           '
+    }
 
     if choice == '1':
         return main_menu
@@ -303,15 +514,19 @@ def menu(choice):
         return prog_menu 
     elif choice == '4':
         return ts_menu 
+    elif choice == '5':
+        return ramp_menu 
+    elif choice == '6':
+        return unit_menu
 
 def print_menu(choice):
     '''set up selection menu
     '''
     print ('\nF4T control options:'
-           '\n--------------------------') 
+           '\n------------------------------') 
     for key in menu(choice).keys():
         print (f'  [{key}]:', menu(choice)[key] )
-    print ('--------------------------') 
+    print ('------------------------------') 
 
 if __name__ == "__main__":
 
@@ -328,6 +543,6 @@ if __name__ == "__main__":
     print (f'\nTemperature Status: \n  PV: {currentPV} \n  SP: {currentSP}')
 
     # initiate menu
-    selection()
+    main_menu()
 
     # sys.exit('Terminated')
